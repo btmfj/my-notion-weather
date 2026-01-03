dfe069da9cc05ba928129a610236bdd9ba8acfce
btmfj <74393836+btmfj@users.noreply.github.com>
2026-01-03 16:44:08 +0900

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -27,7 +27,7 @@ cloudinary.config({
       // 発表時刻から今日のテーブルまでを収めるため、少し高さを調整してスクショ
       await todayTarget.screenshot({ 
         path: 'weather_today.png',
-        clip: { x: 0, y: 0, width: 690, height: 550 } // 発表時刻を含めた上部エリア
+        clip: { x: 0, y: 0, width: 690, height: 450 } // 発表時刻を含めた上部エリア
       });
       const res = await cloudinary.uploader.upload('weather_today.png', {
         public_id: 'weather_today',
